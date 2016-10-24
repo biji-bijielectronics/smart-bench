689cdb94c5f2d539715b51a11125f84bbbac748d
Rohan Ahmad <rohanoahmad@gmail.com>
2016-10-24 16:58:51 +0800

    added basic server and random-data

---
+++ random-data.py
@@ -0,0 +1,31 @@
+## This script just pumps random current and voltage data 
+## onto a redis published 'adc'
+## used for testing socket.io connection
+
+import redis
+import random
+import time
+
+r = redis.Redis()
+
+current = 1
+voltage = 1
+
+def get_current():
+	global current
+	current += (random.random()  - 0.5 ) * 0.5 
+	return round(current,2)
+
+def get_voltage():
+	global voltage
+	voltage += (random.random()  - 0.5 ) * 0.5 
+	return round(voltage,2)
+
+
+while True:
+	v = get_voltage()
+	c = get_current()
+	msg = {'voltage': v, 'current': c}
+	r.publish('adc', msg)
+	print msg
+	time.sleep(0.5)
---
+++ server.py
@@ -0,0 +1,100 @@
+# set async_mode to 'threading', 'eventlet', 'gevent' or 'gevent_uwsgi' to
+# force a mode else, the best mode is selected automatically from what's
+# installed
+async_mode = 'gevent'
+
+import time
+from flask import Flask, render_template
+import socketio
+
+import redis
+sio = socketio.Server(logger=True, async_mode=async_mode)
+
+app = Flask(__name__)
+app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
+app.config['SECRET_KEY'] = 'secret!'
+thread = None
+r = redis.Redis()
+
+pubsub = r.pubsub()
+pubsub.subscribe(['adc'])
+
+
+    
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    while True:
+        count = 0
+
+        item = pubsub.get_message()
+        if item is not None:
+            count += 1
+            msg = str(item['data'])
+            print "sending: " + msg
+            sio.emit('my response', {'data': msg},
+                 namespace='/test')  
+        sio.sleep(0.01)        
+
+
+@app.route('/')
+def index():
+    global thread
+    if thread is None:
+        thread = sio.start_background_task(background_thread)
+    return render_template('index.html')
+
+
+@sio.on('my event', namespace='/test')
+def test_message(sid, message):
+    sio.emit('my response', {'data': message['data']}, room=sid,
+             namespace='/test')
+
+
+
+
+@sio.on('disconnect request', namespace='/test')
+def disconnect_request(sid):
+    sio.disconnect(sid, namespace='/test')
+
+
+@sio.on('connect', namespace='/test')
+def test_connect(sid, environ):
+    sio.emit('my response', {'data': 'Connected', 'count': 0}, room=sid,
+             namespace='/test')
+
+
+@sio.on('disconnect', namespace='/test')
+def test_disconnect(sid):
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    
+    # if sio.async_mode == 'threading':
+    #     # deploy with Werkzeug
+    #     app.run(threaded=True)
+    # elif sio.async_mode == 'eventlet':
+    #     # deploy with eventlet
+    #     import eventlet
+    #     import eventlet.wsgi
+    #     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    # elif sio.async_mode == 'gevent':
+        # deploy with gevent
+        from gevent import pywsgi
+        try:
+            from geventwebsocket.handler import WebSocketHandler
+            websocket = True
+        except ImportError:
+            websocket = False
+        if websocket:
+            pywsgi.WSGIServer(('', 5000), app,
+                              handler_class=WebSocketHandler).serve_forever()
+        else:
+            pywsgi.WSGIServer(('', 5000), app).serve_forever()
+    # elif sio.async_mode == 'gevent_uwsgi':
+    #     print('Start the application through the uwsgi server. Example:')
+    #     print('uwsgi --http :5000 --gevent 1000 --http-websockets --master '
+    #           '--wsgi-file app.py --callable app')
+    # else:
+    #     print('Unknown async_mode: ' + sio.async_mode)
